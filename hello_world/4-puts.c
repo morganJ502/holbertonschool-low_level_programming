ae1ef93d01207df1a0a07125e3c8f0dd5099fe4c
morganJ502 <12124@holbertonstudents.com>
2026-02-25 10:21:00 +0000

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Point d'entrée du programme
+ *
+ * Description: Affiche une phrase spécifique à l'aide de la fonction puts.
+ * Return: Toujours 0 (Succès)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
